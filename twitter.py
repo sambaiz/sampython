3626a739ec7e7a2dc2e033d3d46111718c57eca0
www <www@www.edp.to>
2013-04-19 22:22:56 +0900

    search add

--- twitter.py
+++ twitter.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import tweepy
-import twitter_key                                                               
-
+import twitter_key
+import urllib                                                               
 consumer_key = twitter_key.C_KEY
 consumer_secret = twitter_key.C_SECRET
 access_key = twitter_key.A_KEY
@@ -14,5 +14,13 @@ auth.set_access_token(access_key, access_secret)
 # create API                                                                
 api = tweepy.API(auth_handler=auth)                                         
                                                                             
-# post kani                                                                 
-api.update_status("test")   
+# post kani
+#for i in range(10):
+#  api.update_status('%d 回目' % i)
+
+# 検索
+results = api.search(urllib.quote_plus("foo"))
+
+# 結果を整形
+for result in results:
+    print result.text # とりあえず表示するだけ
